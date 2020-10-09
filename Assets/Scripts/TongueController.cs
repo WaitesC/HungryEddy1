cadfc2a30c7c042ab8465e140c91dc366fdf60cb
Chris <cwaites@hotmail.com>
2020-10-10 00:05:30 +1100

    Implemented most of the basic animations

--- Assets/Scripts/TongueController.cs
+++ Assets/Scripts/TongueController.cs
@@ -14,17 +14,57 @@ public class TongueController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //tongue normal
         if (Input.GetButtonDown("Tongue"))
         {
             Tongue();
 
         }
+
+        //tongue up
+        if (Input.GetButtonDown("Tongue") && Input.GetAxisRaw("Vertical") > 0) 
+        {
+            TongueUp();
+
+        }
+        
+        //tongue down
+        if (Input.GetButtonDown("Tongue") && Input.GetAxisRaw("Vertical") < 0) 
+        {
+            TongueDown();
+
+        }
+
+
     }
 
     void Tongue()
     {
         animator.SetTrigger("TongueNormal");
+
+        Collider2D[] hitCaterpillars = Physics2D.OverlapCircleAll(tonguePoint.position, tongueRange, caterpillarLayers);
+
+        foreach(Collider2D caterpillar in hitCaterpillars)
+        {
+            Debug.Log("We hit " + caterpillar.name);
+        }
+    }
+    
+    void TongueUp()
+    {
+        animator.SetTrigger("TongueUp");
+
+        Collider2D[] hitCaterpillars = Physics2D.OverlapCircleAll(tonguePoint.position, tongueRange, caterpillarLayers);
+
+        foreach(Collider2D caterpillar in hitCaterpillars)
+        {
+            Debug.Log("We hit " + caterpillar.name);
+        }
+    }
+    
+    void TongueDown()
+    {
+        animator.SetTrigger("TongueDown");
 
         Collider2D[] hitCaterpillars = Physics2D.OverlapCircleAll(tonguePoint.position, tongueRange, caterpillarLayers);
 
@@ -42,4 +82,6 @@ public class TongueController : MonoBehaviour
         Gizmos.DrawWireSphere(tonguePoint.position, tongueRange);
 
     }
+
+
 }
