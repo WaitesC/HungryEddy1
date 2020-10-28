464ecaa8bc76a56ed6850e9d6a9830f23688968e
Cecile Roux <croux@laureate.net.au>
2020-10-28 17:07:32 +0100

    Cloud particle on impact test 1

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    [SerializeField]
+    GameObject dustCloud;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Ground"))
+            Instantiate(dustCloud, transform.position, dustCloud.transform.rotation);
+    }
+
+}
+
