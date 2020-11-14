39691bcae5e78f54f17f8cd5eee8cbba5f7db7c7
Chris <cwaites@hotmail.com>
2020-11-14 16:18:13 +1100

    Main menu works with controller

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 
@@ -15,171 +16,92 @@ public class MenuController : MonoBehaviour
     public GameObject CreditsObj;
     public GameObject VolumeObj;
 
-    //public Animator transitionAnimator;
+    public GameObject splashConfirmButton, newGameButton, controllerButton, controllerReturnButton, quitYesButton, creditsReturnButton, volumeReturnButton;
 
-    GameObject transition;
+    public string firstLevel;
 
-    // Start is called before the first frame update
+
+    //GameObject transition;
+
     void Start()
     {
-        //transitionAnimator = GameObject.Find("Transition").GetComponent<Animator>();
-        //transition = GameObject.Find("Transition");
-
-        //StudioPage();
-        SplashLogo();
-        //TransitionStart();
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         
-    }
-
-    void TransitionStart()
-    {
-        //do transition stuff
-        //transitionAnimator.Play("TransitionAnimationStart");
-    }
-
-    void TransitionFull()
-    {
+        SplashLogo();
 
     }
+
     
-    void TransitionEnd()
-    {
-        //do transition stuff
-    }
 
-    void TransitionEmpty()
-    {
-
-    }
-
-    public void StudioPage()
-    {
-        //StudioPageObj.SetActive(true);
-        SplashLogoObj.SetActive(false);
-        MainMenuObj.SetActive(false);
-        SettingsObj.SetActive(false);
-        ControllerObj.SetActive(false);
-        QuitObj.SetActive(false);
-        CreditsObj.SetActive(false);
-        VolumeObj.SetActive(false);
-
-
-        Invoke("SplashLogo", 7.0f);
-    }
+    
 
     public void SplashLogo()
     {
-        //transition.SetActive(false);
-
-        //StudioPageObj.SetActive(false);
+        clearScreen();
         SplashLogoObj.SetActive(true);
-        MainMenuObj.SetActive(false);
-        SettingsObj.SetActive(false);
-        ControllerObj.SetActive(false);
-        QuitObj.SetActive(false);
-        CreditsObj.SetActive(false);
-        VolumeObj.SetActive(false);
-
-
+        //EventSystem.current.SetSelectedGameObject(splashConfirmButton);
 
     }
 
     public void MainMenu()
     {
-        //StudioPageObj.SetActive(false);
-        SplashLogoObj.SetActive(false);
+        clearScreen();
         MainMenuObj.SetActive(true);
-        SettingsObj.SetActive(false);
-        ControllerObj.SetActive(false);
-        QuitObj.SetActive(false);
-        CreditsObj.SetActive(false);
-        VolumeObj.SetActive(false);
-
-
+        EventSystem.current.SetSelectedGameObject(newGameButton);
     }
 
     public void Settings()
     {
-        //StudioPageObj.SetActive(false);
-        SplashLogoObj.SetActive(false);
-        MainMenuObj.SetActive(false);
+        clearScreen();
         SettingsObj.SetActive(true);
-        ControllerObj.SetActive(false);
-        QuitObj.SetActive(false);
-        CreditsObj.SetActive(false);
-        VolumeObj.SetActive(false);
-
-
+        EventSystem.current.SetSelectedGameObject(controllerButton);
     }
 
     public void Controller()
     {
-        //StudioPageObj.SetActive(false);
-        SplashLogoObj.SetActive(false);
-        MainMenuObj.SetActive(false);
-        SettingsObj.SetActive(false);
+        clearScreen();
         ControllerObj.SetActive(true);
-        QuitObj.SetActive(false);
-        CreditsObj.SetActive(false);
-        VolumeObj.SetActive(false);
-
-
+        EventSystem.current.SetSelectedGameObject(controllerReturnButton);
     }
 
     public void Quit()
     {
-        //StudioPageObj.SetActive(false);
-        SplashLogoObj.SetActive(false);
-        MainMenuObj.SetActive(false);
-        SettingsObj.SetActive(false);
-        ControllerObj.SetActive(false);
+        clearScreen();
         QuitObj.SetActive(true);
-        CreditsObj.SetActive(false);
-        VolumeObj.SetActive(false);
-
-
+        EventSystem.current.SetSelectedGameObject(quitYesButton);
     }
 
     public void Credits()
     {
-        //StudioPageObj.SetActive(false);
-        SplashLogoObj.SetActive(false);
-        MainMenuObj.SetActive(false);
-        SettingsObj.SetActive(false);
-        ControllerObj.SetActive(false);
-        QuitObj.SetActive(false);
+        clearScreen();
         CreditsObj.SetActive(true);
-        VolumeObj.SetActive(false);
-
+        EventSystem.current.SetSelectedGameObject(creditsReturnButton);
 
     }
 
     public void Volume()
     {
-        //StudioPageObj.SetActive(false);
+        clearScreen();
+        VolumeObj.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(volumeReturnButton);
+
+    }
+
+    public void clearScreen()
+    {
+        //EventSystem.current.SetSelectedGameObject(null);
         SplashLogoObj.SetActive(false);
         MainMenuObj.SetActive(false);
         SettingsObj.SetActive(false);
         ControllerObj.SetActive(false);
         QuitObj.SetActive(false);
         CreditsObj.SetActive(false);
-        VolumeObj.SetActive(true);
-
-
+        VolumeObj.SetActive(false);
     }
-
-
 
 
     public void NewGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene(firstLevel);
 
     }
 }
