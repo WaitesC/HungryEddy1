c244408a3ac23abbe53c873514cda8d1dd83f570
Cecile Roux <croux@laureate.net.au>
2020-11-19 06:00:45 +0100

    New type writer effect for UI tuto

---
+++ Assets/Scripts/TextWriter.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWriter : MonoBehaviour
+{
+    private TextWriterSingle textWriterSingle;
+
+  public void AddWriter(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
+    {
+        textWriterSingle = new TextWriterSingle(uiText, textToWrite, timePerCharacter, invisibleCharacters);
+    }
+
+    public void Update()
+    {
+        if (textWriterSingle != null)
+        {
+            textWriterSingle.Update();
+        }
+    }
+
+    // Represents a single TextWriter instance
+    public class TextWriterSingle
+    {
+        private Text uiText;
+        private string textToWrite;
+        private int characterIndex;
+        private float timePerCharacter;
+        private float timer;
+        private bool invisibleCharacters;
+
+        public TextWriterSingle(Text uiText, string textToWrite, float timePerCharacter, bool invisibleCharacters)
+        {
+            this.uiText = uiText;
+            this.textToWrite = textToWrite;
+            this.timePerCharacter = timePerCharacter;
+            this.invisibleCharacters = invisibleCharacters;
+            characterIndex = 0;
+        }
+        
+        
+        public void Update()
+        {
+            if (uiText != null)
+            {
+                timer -= Time.deltaTime;
+                while (timer <= 0f)
+                {
+                    // Display next character
+                    timer += timePerCharacter;
+                    characterIndex++;
+                    string text = textToWrite.Substring(0, characterIndex);
+                    if (invisibleCharacters)
+                    {
+                        text += "<color=#00000000>" + textToWrite.Substring(characterIndex) + "</color>";
+                    }
+                    uiText.text = text;
+
+                    if (characterIndex >= textToWrite.Length)
+                    {
+                        // Entire string displayed
+                        uiText = null;
+                        return;
+                    }
+                }
+            }
+
+        }
+    }
+}
+
---
+++ Assets/Scripts/UIBubbleText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBubbleText : MonoBehaviour
+{
+    [SerializeField] private TextWriter textWriter;
+    private Text messageText;
+    private void Awake()
+    {
+        messageText = transform.Find("message").Find("messageText").GetComponent<Text>();
+        //Application.targetFrameRate = 3;
+    }
+    private void Start() 
+    {
+        //textWriter.AddWriter(messageText, "Hello World", 1f);
+        textWriter.AddWriter(messageText, "Hello, I'm Eddy", 1f, true);
+    }
+
+}
