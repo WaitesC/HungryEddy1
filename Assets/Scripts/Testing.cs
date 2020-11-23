95cce1d3f35b4f93ad1328d07af520c2890d220e
Cecile Roux <croux@laureate.net.au>
2020-11-23 15:07:44 +0100

    Pop up text for scoring test 1

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] private Transform ScorePopUp;
+    
+    private void Start()
+    {
+        Instantiate(ScorePopup, Vector3.zero, Quaternion.identity);
+    }
+
+
+}
